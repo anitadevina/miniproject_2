99469289cdc396ccce5da642c2dfccc87cbb19d8
Yeremi Andrew <kurikarakenjou@gmail.com>
2024-12-03 22:41:42 +0700

    fix: adjust query

--- data_warehouse/models/mart/best_selling_film.sql
+++ data_warehouse/models/mart/best_selling_film.sql
@@ -1,13 +1,13 @@
 {{ config(materialized='table', schema='mart') }}
 
 -- i need to know the factor to answer the problem, currently i use my perception
-SELECT
-    MIN(df.film_id) AS film_id, 
-    df.title, 
-    COUNT(dr.*)::integer AS total_rent
+SELECT 
+    df.title AS film_title, 
+    SUM(fp.amount) AS total_sales
 FROM {{ ref('fact_payment') }} fp
 LEFT JOIN {{ ref('dim_rental') }} dr ON fp.rental_id = dr.rental_id
 LEFT JOIN {{ ref('dim_inventory') }} di ON dr.inventory_id = di.inventory_id
 LEFT JOIN {{ ref('dim_film') }} df ON di.film_id = df.film_id
 GROUP BY df.title
-ORDER BY COUNT(dr.*)::integer DESC
+ORDER BY total_sales DESC
+LIMIT 1
