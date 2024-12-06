52afcf18dc60ecdb79f675e2ec01feffcea8d44b
anitadevina <anitadevina8041@gmail.com>
2024-12-06 22:43:12 +0700

    edit query for question num 1

--- data_warehouse/models/mart/monthly_revenue.sql
+++ data_warehouse/models/mart/monthly_revenue.sql
@@ -4,6 +4,6 @@ SELECT
     to_char(fp.payment_date, 'FMMonth YYYY') AS month_year,
     SUM(fp.amount) AS total_revenue
 FROM {{ ref('fact_payment') }} fp
-LEFT JOIN {{ ref('dim_rental') }} dr ON fp.rental_id = dr.rental_id
+--LEFT JOIN {{ ref('dim_rental') }} dr ON fp.rental_id = dr.rental_id
 GROUP BY month_year
-ORDER BY total_revenue DESC
+ORDER BY MIN(fp.payment_date)
